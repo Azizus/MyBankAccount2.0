dcd95e7a1adbbe940ddd0be131efb3c80fdf2a09
saidi aziz <user@192.168.0.17>
2019-03-27 14:28:08 +0100

    FAIL : DateFormatTest { return_date_with_format_ddMMyyyy(); }

---
+++ src/main/java/com/kata/bankaccount/utils/DateFormat.java
@@ -0,0 +1,12 @@
+package com.kata.bankaccount.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormat {
+
+	public String formatDateToString(Date date) {
+		return null;
+	}
+	
+}
