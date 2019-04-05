f5b8e59a709a27a9cba268e362de8b9d5177d291
saidi aziz <user@192.168.0.17>
2019-04-05 18:47:01 +0200

    REAFACTOR : push integration test

--- src/test/java/com/kata/bankaccount/controller/TransactionControllerTest.java
+++ src/test/java/com/kata/bankaccount/controller/TransactionControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.kata.bankaccount.domain.Account;
+import com.kata.bankaccount.exceptions.TransactionException;
 import com.kata.bankaccount.service.AccountService;
 import com.kata.bankaccount.service.TransactionService;
 import io.restassured.RestAssured;
@@ -57,9 +58,7 @@ public class TransactionControllerTest {
   public void should_create_withdraw_transaction() throws AccountException {
 
 
-    // Account account2 = new Account(2, 100);
     accountService.save(account);
-    // long account2Id = account2.getAccountId();
     int withdrawAmount = 70;
     given().contentType("application/json")//
         .when().post("/transactions/" + account.getAccountId() + "/withdraw/" + withdrawAmount + "")//
@@ -72,19 +71,16 @@ public class TransactionControllerTest {
   }
 
 
-  // @Test
-  // public void should_print_statement_of_account() throws AccountException, TransactionException {
-  // accountService.save(account);
-  // transactionService.deposit(account.getAccountId(), 100);
-  // transactionService.withdraw(account.getAccountId(), 50);
-  // transactionService.deposit(account.getAccountId(), 200);
-  //
-  // given().contentType("application/json")//
-  // .when().post("/transactions/" + account.getAccountId() + "")//
-  // .then().statusCode(200);//
-  // // .assertThat().body("accountId", is(equalTo(1)))//
-  // // .assertThat().body("balance", is(equalTo(30)))//
-  // // .assertThat().body("amount", is(equalTo(70)))//
-  // // .assertThat().body("type", is(equalTo("WITHDRAWAL")));//
-  // }
+  @Test
+  public void should_print_statement_of_account() throws AccountException, TransactionException {
+    accountService.save(account);
+
+    transactionService.deposit(account.getAccountId(), 100);
+    transactionService.withdraw(account.getAccountId(), 50);
+
+
+    given().contentType("application/json")//
+        .when().get("/transactions/" + account.getAccountId() + "")//
+        .then().statusCode(200);//
+  }
 }
